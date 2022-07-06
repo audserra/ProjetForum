ec8c04b216edc7c62882248957e42712adff4214
Audrey Serra <audrey.serra@free.fr>
2022-07-06 15:24:49 +0200

    Ajout de CommentService

---
+++ forum-api/Services/CommentService.cs
@@ -0,0 +1,37 @@
+﻿using forum_api.DataAccess.DataObjects;
+using forum_api.Repositories;
+
+namespace forum_api.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _repository;
+
+        public CommentService(ICommentRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public Comment GetCommentById(int id)
+        {
+            return this._repository.GetCommentById(id);
+        }
+        public List<Comment> GetCommentsByTopicId(int topicId)
+        {
+            return this._repository.GetCommentsByTopicId(topicId);
+        }
+        public void CreateComment(Comment comment)
+        {
+            this._repository.CreateComment(comment);
+        }
+        public void UpdateComment(int id, Comment comment)
+        {
+            this._repository.UpdateComment(id, comment);
+        }
+
+        public void DeleteComment(int id)
+        {
+            this._repository.DeleteComment(id);
+        }
+    }
+}
---
+++ forum-api/Services/ICommentService.cs
@@ -0,0 +1,13 @@
+﻿using forum_api.DataAccess.DataObjects;
+
+namespace forum_api.Services
+{
+    public interface ICommentService
+    {
+        void CreateComment(Comment comment);
+        void DeleteComment(int id);
+        Comment GetCommentById(int id);
+        List<Comment> GetCommentsByTopicId(int topicId);
+        void UpdateComment(int id, Comment comment);
+    }
+}
