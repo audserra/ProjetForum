e3b9f48fe9b4e8bdc818cec84b58f8a425ed6241
Audrey Serra <audrey.serra@free.fr>
2022-07-06 15:34:03 +0200

    Ajout CommentsController

---
+++ forum-api/Controllers/CommentsController.cs
@@ -0,0 +1,52 @@
+﻿using forum_api.DataAccess.DataObjects;
+using forum_api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace forum_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentService _service;
+
+        public CommentsController(ICommentService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCommentById(int id)
+        {
+            return Ok(this._service.GetCommentById(id));
+        }
+
+        [HttpGet("topic/{topicId}")]
+        public IActionResult GetCommentsByyTopicId(int topicId)
+        {
+            return Ok(this._service.GetCommentsByTopicId(topicId));
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(Comment comment)
+        {
+            this._service.CreateComment(comment);
+            return Ok("Created!");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateComment(int id, Comment comment)
+        {
+            this._service.UpdateComment(id, comment);
+            return Ok("Updated!");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteComment(int id)
+        {
+            this._service.DeleteComment(id);
+            return Ok("Deleted!");
+        }
+    }
+}
