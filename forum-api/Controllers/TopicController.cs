1218e1a4582ec4051bbf6e1e1f4f8422f4c7a6af
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-07-06 16:24:31 +0200

    Ajout de Topic

---
+++ forum-api/Controllers/TopicController.cs
@@ -0,0 +1,56 @@
+﻿using forum_api.DataAccess.DataObjects;
+using forum_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace forum_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TopicController : ControllerBase
+    {
+        private readonly ITopicService topicService;
+
+        public TopicController(ITopicService service)
+        {
+            topicService = service;
+        }
+
+        [HttpGet("{id}")]
+        public Topic GetTopicById(int id)
+        {
+            return this.topicService.GetTopicById(id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Topic> GetTopics()
+        {
+            return this.topicService.GetTopics();
+        }
+
+        [HttpPost]
+        public Topic AddTopic(Topic topic)
+        {
+            return this.topicService.AddTopic(topic);
+        }
+
+        [HttpPut]
+        public Topic UpdateTopic(Topic topic)
+        {
+            return this.topicService.UpdateTopic(topic);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTopic(int id)
+        {
+            try 
+            {
+                this.topicService.DeleteTopic(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return  BadRequest("Invalide request" + e.Message);
+            }
+        }
+    }
+}
---
+++ forum-api/Services/ITopicService.cs
@@ -0,0 +1,13 @@
+﻿using forum_api.DataAccess.DataObjects;
+
+namespace forum_api.Services
+{
+    public interface ITopicService
+    {
+        Topic AddTopic(Topic topic);
+        void DeleteTopic(int id);
+        Topic GetTopicById(int id);
+        IEnumerable<Topic> GetTopics();
+        Topic UpdateTopic(Topic topic);
+    }
+}
