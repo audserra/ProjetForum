0994a185a5bfa929a569a0971df7a0010c39bc49
Audrey Serra <audrey.serra@free.fr>
2022-07-07 10:16:45 +0200

    Tests unitaires CRUD Comments

--- forum-apiTests/Services/CommentServiceTests.cs
+++ forum-apiTests/Services/CommentServiceTests.cs
@@ -18,6 +18,7 @@ namespace forum_api.Services.Tests
         private ICommentService _commentService;
         private Mock<ICommentRepository> _commentRepository;
         private Comment expectedComment;
+        private DateTime originDateTime;
 
         [TestInitialize]
         public void Initialize()
@@ -25,12 +26,13 @@ namespace forum_api.Services.Tests
             this._commentRepository = new Mock<ICommentRepository>(MockBehavior.Strict);
             this._commentService = new CommentService(this._commentRepository.Object);
 
+            this.originDateTime = new DateTime(1970, 1, 1);
             this.expectedComment = new Comment()
             {
                 Id = 1,
                 User = Guid.NewGuid().ToString(),
-                CreationDate = DateTime.Now,
-                ModificationDate = DateTime.Now,
+                CreationDate = originDateTime,
+                ModificationDate = originDateTime,
                 Content = Guid.NewGuid().ToString(),
                 TopicId = 1
             };
@@ -68,38 +70,92 @@ namespace forum_api.Services.Tests
             this._commentRepository.VerifyAll();
         }
 
-        //[TestMethod()]
-        //public void GetCommentsByTopicIdTest_ValidTopicId_ShouldReturnList()
-        //{
-        //    // Arrange
-        //    this._commentRepository.Setup(r => r.GetCommentsByTopicId(It.IsAny<int>()))
-        //        .Returns(new List<Comment>() { expectedComment, expectedComment});
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void GetCommentsByTopicIdTest_ValidTopicId_ShouldReturnList(int id)
+        {
+            // Arrange
+            this._commentRepository.Setup(r => r.GetCommentsByTopicId(It.IsAny<int>()))
+                .Returns(new List<Comment>() { expectedComment, expectedComment});
 
-        //    // Act
-        //    var actualComments = _commentService.GetCommentById(id);
+            // Act
+            var actualComments = _commentService.GetCommentsByTopicId(id);
 
-        //    // Assert
-        //    Assert.IsInstanceOfType(actualComment, typeof(Comment));
-        //    Assert.AreEqual(expectedComment, actualComment);
-        //    this._commentRepository.VerifyAll();
-        //}
+            // Assert
+            Assert.IsInstanceOfType(actualComments, typeof(List<Comment>));
+            Assert.AreEqual(2, actualComments.Count);
+            this._commentRepository.VerifyAll();
+        }
 
-        //[TestMethod()]
-        //public void CreateCommentTest()
-        //{
-        //    Assert.Fail();
-        //}
+        [TestMethod()]
+        public void CreateCommentTest_ValidInput_ShouldUpdateCreationDateAndAddComment()
+        {
+            // Arrange
+            this._commentRepository.Setup(r => r.CreateComment(It.IsAny<Comment>()))
+                .Returns(expectedComment);
 
-        //[TestMethod()]
-        //public void UpdateCommentTest()
-        //{
-        //    Assert.Fail();
-        //}
+            // Act
+            var actualComment = _commentService.CreateComment(expectedComment);
 
-        //[TestMethod()]
-        //public void DeleteCommentTest()
-        //{
-        //    Assert.Fail();
-        //}
+            // Assert
+            Assert.IsInstanceOfType(actualComment, typeof(Comment));
+            Assert.AreEqual(expectedComment, actualComment);
+            Assert.AreNotEqual(originDateTime, actualComment.CreationDate);
+            this._commentRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void UpdateCommentTest_ValidInput_ShouldUpdateModificationDateAndUpdateComment()
+        {
+            // Arrange
+            this._commentRepository.Setup(r => r.UpdateComment(It.IsAny<Comment>()))
+                .Returns(expectedComment);
+
+            // Act
+            var actualComment = _commentService.UpdateComment(expectedComment);
+
+            // Assert
+            Assert.IsInstanceOfType(actualComment, typeof(Comment));
+            Assert.AreEqual(expectedComment, actualComment);
+            Assert.AreEqual(originDateTime, actualComment.CreationDate);
+            Assert.AreNotEqual(originDateTime, actualComment.ModificationDate);
+            this._commentRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void DeleteCommentTest_ValidId_ShouldDeleteComment(int id)
+        {
+            // Arrange
+            this._commentRepository.Setup(r => r.GetCommentById(It.IsAny<int>()))
+                .Returns(this.expectedComment);
+
+            this._commentRepository.Setup(r => r.DeleteComment(It.IsAny<int>()));
+
+            // Act
+            _commentService.DeleteComment(id);
+
+            // Assert
+            this._commentRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void DeleteCommentTest_InvalidId_ShouldThrowNotFoundException(int id)
+        {
+            // Arrange
+            this._commentRepository.Setup(r => r.GetCommentById(It.IsAny<int>()))
+                .Returns((Comment)null);
+
+            // Act + Assert
+            Assert.ThrowsException<NotFoundException>(() => _commentService.DeleteComment(id));
+            this._commentRepository.VerifyAll();
+        }
+
+
+
     }
 }
