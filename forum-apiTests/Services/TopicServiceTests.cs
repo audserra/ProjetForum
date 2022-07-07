6d16546ed1b805fd58e2b87bec9bec130ecd4693
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-07-07 10:12:39 +0200

    Add tests on topic service

---
+++ forum-apiTests/Services/TopicServiceTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using forum_api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using forum_api.Repositories;
+using Moq;
+using forum_api.DataAccess.DataObjects;
+using forum_api.Exceptions;
+
+namespace forum_api.Services.Tests
+{
+    [TestClass()]
+    public class TopicServiceTests
+    {
+        private ITopicService _topicService;
+        private Mock<ITopicRepository> _topicRepository;
+        private Topic expectedTopic;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._topicRepository = new Mock<ITopicRepository>();
+            this._topicService = new TopicService(this._topicRepository.Object);
+
+            this.expectedTopic = new Topic()
+            {
+                Id = 1,
+                CreationDate = DateTime.Now,
+                ModificationDate = null,
+                Title = "Test",
+                Author = "Thomas",
+                Comments = new List<Comment>()
+            };
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void GetTopicByIdTest_ParamsOk_ShouldReturnTopic(int id)
+        {
+            // Arrange
+            this._topicRepository.Setup(repo => repo.GetTopicById(It.IsAny<int>())).Returns(this.expectedTopic);
+
+            // Act
+            Topic actualTopic = _topicService.GetTopicById(id);
+
+            // Assert
+            Assert.IsInstanceOfType(actualTopic, typeof(Topic));
+            Assert.AreEqual(expectedTopic, actualTopic);
+            this._topicRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void GetTopicByIdTest_InvalidId_ShouldThrowNotFoundException(int id)
+        {
+            // Arrange
+            this._topicRepository.Setup(r => r.GetTopicById(It.IsAny<int>()))
+                .Returns((Topic)null);
+
+            // Act + Assert
+            Assert.ThrowsException<NotFoundException>(() => _topicService.GetTopicById(id));
+            this._topicRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void GetTopicsTest_ParamsOk_ShouldReturnList()
+        {
+            // Arrange
+            List<Topic> expectedTopics = new List<Topic>() { expectedTopic, expectedTopic };
+            this._topicRepository.Setup(r => r.GetTopics()).Returns(expectedTopics);
+
+            // Act
+            var actualTopics = _topicService.GetTopics();
+
+            // Assert
+            Assert.IsInstanceOfType(actualTopics, typeof(List<Topic>));
+            Assert.AreEqual(expectedTopics, actualTopics);
+            this._topicRepository.VerifyAll();
+        }
+
+        //[TestMethod()]
+        //public void CreateTopicTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void UpdateTopicTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void DeleteTopicTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
