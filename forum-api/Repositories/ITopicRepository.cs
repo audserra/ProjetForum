9a09f2dad98080669e207a8e71752b6a1d7392fe
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-07-07 09:30:20 +0200

    mise en forme exception

--- forum-api/Repositories/ITopicRepository.cs
+++ forum-api/Repositories/ITopicRepository.cs
@@ -5,7 +5,7 @@ namespace forum_api.Repositories
     public interface ITopicRepository
     {
         Topic AddTopic(Topic topic);
-        Topic DeleteTopic(int id);
+        void DeleteTopic(int id);
         Topic GetTopicById(int id);
         IEnumerable<Topic> GetTopics();
         Topic UpdateTopic(Topic topic);
